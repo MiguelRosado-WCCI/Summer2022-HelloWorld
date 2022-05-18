c2049995519b8b8162075b366b2ef2bce20b3c16
MiguelRosado-WCCI <M.rosado1725@gmail.com>
2022-05-18 14:42:37 -0400

    visual studios day 1

---
+++ Summer2022-HelloWorld/Summer2022-HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string instructor;
+instructor = null;
+Console.WriteLine(instructor);
+instructor = "Davis";
+string coInstructor = "Gavin";
+
+int studentCount = 8;
+string input = Console.ReadLine();
+
+Console.WriteLine(input);
